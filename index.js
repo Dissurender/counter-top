0329241823dc601645be7f6c84c405e03f64d9c2
diss <65290988+Dissurender@users.noreply.github.com>
2024-02-24 20:02:08 -0600

    refactor away from IIFE

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import { parseArgs } from "node:util";
 import { readFileSync } from "node:fs";
 
-(async function main() {
+const num = [0];
+async function main() {
   const {
     values: { file, word, line, bytes },
   } = parseArgs({
@@ -29,6 +30,10 @@ import { readFileSync } from "node:fs";
     },
   });
 
+  const args = process.argv.slice(2);
+  const options = { word, line, bytes };
+
+  num[0]++;
   if (file === "not") {
     console.error(
       "Incorrect number of arguments.\n" +
@@ -40,8 +45,11 @@ import { readFileSync } from "node:fs";
   }
 
   try {
+    const test = console.log(file);
     const data = await readFileSync(file, "utf8");
+    const test2 = console.log(file);
 
+    ++num[0];
     if (line) {
       countLine(data);
     }
@@ -54,10 +62,14 @@ import { readFileSync } from "node:fs";
   } catch (error) {
     console.error("Error reading file: " + file + "\n\nExiting process...");
     console.error(error.message);
+    console.error(error.stack);
 
+    // Exit process with error code 1
     process.exit(1);
   }
-})();
+
+  return 0;
+}
 
 const countLine = (data) => {
   console.log(data.split("\n").length, "lines");
@@ -70,3 +82,5 @@ const countWord = (data) => {
 const countBytes = (data) => {
   console.log(data.length, "bytes");
 };
+
+console.log(await main());
